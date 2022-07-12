c0365198b4a75241bcf639d9975b658484752198
Fausto Rossi <76191699+FaustoRossi@users.noreply.github.com>
2022-07-12 06:24:20 -0300

    Update SliderData.js

--- components/SliderData.js
+++ components/SliderData.js
@@ -22,10 +22,10 @@ export const SliderData = [
 	{
 		destination: "",
 		image:
-			"https://images.unsplash.com/photo-1604626676599-e21a94462216?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aWd1YXp1fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+			"https://media.istockphoto.com/photos/argentina-iguazu-waterfalls-garganta-del-diablo-with-rainbow-picture-id155384430?b=1&k=20&m=155384430&s=612x612&w=0&h=8FXQExVzIElt9-httZOH7t-gOubmZyPm3czd2e4Nadk=",
 	},
 	{
-		destination: "asd",
+		destination: "",
 		image:
 			"https://images.unsplash.com/photo-1582042155253-8cdf75c97f86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
 	},
